dc126064529ca12c96b7ca1755a38f24379390fe
Mark McKelvy <Mark.McKelvy@UTSouthwestern.edu>
2025-07-07 15:01:34 -0500

    fixing market 2

--- app/market/page.tsx
+++ app/market/page.tsx
@@ -1,16 +1,25 @@
+'use server'
+
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 import Image from 'next/image'
 
 export default async function MarketPage() {
   const cookieStore = cookies()
+
   const supabase = createServerClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  {
-    cookies: cookieStore
-  }
-)
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name) {
+          return cookieStore.get(name)?.value ?? ''
+        },
+        set() {},
+        remove() {},
+      },
+    }
+  )
 
   const { data: properties, error } = await supabase
     .from('properties')
