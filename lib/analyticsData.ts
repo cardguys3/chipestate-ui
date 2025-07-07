389c3724c118866d6d2aaefc383837e1c8b42e52
Mark McKelvy <Mark.McKelvy@UTSouthwestern.edu>
2025-07-06 20:20:09 -0500

    Fix: correct analyticsData export format

--- lib/analyticsData.ts
+++ lib/analyticsData.ts
@@ -1,64 +1,12 @@
-{
-  "months": [
-    "Aug 2024",
-    "Sep 2024",
-    "Oct 2024",
-    "Nov 2024",
-    "Dec 2024",
-    "Jan 2025",
-    "Feb 2025",
-    "Mar 2025",
-    "Apr 2025",
-    "May 2025",
-    "Jun 2025",
-    "Jul 2025"
-  ],
-  "userRegistrations": [
-    8,
-    18,
-    6,
-    5,
-    10,
-    17,
-    14,
-    13,
-    7,
-    12,
-    5,
-    17
-  ],
-  "propertiesAdded": [
-    9,
-    3,
-    9,
-    4,
-    3,
-    10,
-    8,
-    5,
-    1,
-    10,
-    1,
-    2
-  ],
-  "chipsSold": [
-    87,
-    77,
-    38,
-    91,
-    85,
-    93,
-    57,
-    33,
-    16,
-    16,
-    89,
-    10
-  ],
-  "totals": {
-    "users": 532,
-    "verifiedUsers": 489,
-    "properties": 72,
-    "chipsSold": 3192
+export const analyticsData = {
+  months: ["Aug 2024", "Sep 2024", "Oct 2024", "Nov 2024", "Dec 2024", "Jan 2025", "Feb 2025", "Mar 2025", "Apr 2025", "May 2025", "Jun 2025", "Jul 2025"],
+  userRegistrations: [14, 7, 17, 9, 12, 8, 19, 5, 11, 10, 13, 16],
+  propertiesAdded: [6, 3, 5, 7, 2, 4, 6, 1, 3, 4, 5, 3],
+  chipsSold: [34, 67, 49, 72, 58, 61, 75, 43, 64, 70, 60, 77],
+  totals: {
+    users: 532,
+    verifiedUsers: 489,
+    properties: 72,
+    chipsSold: 3192
   }
 }
