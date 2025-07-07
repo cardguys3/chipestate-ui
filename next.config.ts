19d8acc0b9a885833916be32ee623ebb6a8eba73
Mark McKelvy <Mark.McKelvy@UTSouthwestern.edu>
2025-07-07 15:35:53 -0500

    fixing images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['ajburehyunbvpuhnyjbo.supabase.co'], // ✅ Replace with your actual Supabase project domain if different
+  },
+}
 
-export default nextConfig;
+export default nextConfig
