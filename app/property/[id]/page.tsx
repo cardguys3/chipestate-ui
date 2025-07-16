160b08e4810a7198961a840b72d33684b10d66cc
Mark McKelvy <Mark.McKelvy@UTSouthwestern.edu>
2025-07-16 12:07:32 -0500

    fix login landing page 32

--- app/property/[id]/page.tsx
+++ app/property/[id]/page.tsx
@@ -1,7 +1,6 @@
 // app/property/[id]/page.tsx
 
 import { cookies } from 'next/headers'
-
 import { redirect } from 'next/navigation'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import type { Metadata } from 'next'
