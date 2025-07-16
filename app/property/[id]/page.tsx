11b02fbb7364888c3271c63f76b975e2e3a0da9a
Mark McKelvy <Mark.McKelvy@UTSouthwestern.edu>
2025-07-16 10:15:52 -0500

    fix login landing page 12

--- app/property/[id]/page.tsx
+++ app/property/[id]/page.tsx
@@ -1,6 +1,7 @@
 // app/property/[id]/page.tsx
 
 import { cookies } from 'next/headers'
+
 import { redirect } from 'next/navigation'
 import { createServerClient } from '@supabase/ssr'
 import { Metadata } from 'next'
