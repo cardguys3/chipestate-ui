5beebf8ae254a9e84edb05da751f2925ca6460b8
Mark McKelvy <Mark.McKelvy@UTSouthwestern.edu>
2025-07-21 11:39:40 -0500

    Fix market login issue 10.10

--- app/register/license/page.tsx
+++ app/register/license/page.tsx
@@ -190,8 +190,9 @@ useEffect(() => {
 		  )
 		}
 
-		return (
-		  <main className="min-h-screen bg-blue-950 text-white p-6 flex flex-col justify-between">
+		        return (
+          <main className="min-h-screen bg-blue-950 text-white p-6 flex flex-col justify-between">
+
 			<div>
 			  {/* Progress Graphic */}
         <div className="mb-6 text-sm font-medium text-center text-gray-300 border border-emerald-700 px-4 py-2 rounded w-fit mx-auto">
